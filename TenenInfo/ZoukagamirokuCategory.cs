52fb6e1f8d709ff946b0a3aa1917e5862c552b99
虹川郷鬼 <nijikawasatoki00@outlook.com>
2024-11-23 14:27:06 -0500

    Created ZoukagamirokuCategory.cs

---
+++ TenenInfo/ZoukagamirokuCategory.cs
@@ -0,0 +1,29 @@
+﻿using Niji.Convenience;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TenenInfo.Person;
+
+namespace TenenInfo {
+    public class ZoukagamirokuCategory {
+        public void Test() {
+            // Initialise objects
+            Convenience convenience = new Convenience();
+            // Initialise variables
+            int choice;
+            // Ask the user for input
+            Console.WriteLine("Whose info do you want?");
+            Console.WriteLine("1. Test-kun");
+            choice = convenience.Input32BitInteger();
+            switch (choice) {
+                case 1:
+                    var testKun = new Test();
+                    testKun.TestKun();
+                    break;
+                // End of switch block
+            }
+        }
+    }
+}
